a7716548f9b206eb1c12fdde4de4b1bad423d692
OgNeto <ogsilvanetu@gmail.com>
2025-10-07 13:17:26 -0300

    First components and partials

---
+++ resources/views/site/layouts/_components/form-component.blade.php
@@ -0,0 +1,32 @@
+{{ $slot }}
+
+<form action='{{ route('form') }}' method='post'>
+
+    @csrf
+    <p> Name:
+        <input name='name' type='text' placeholder='Write the student name here...'>
+    </p>
+
+    <p> Email:
+        <input name='email' type='text' placeholder='Write the student email here...'>
+    </p>
+
+    <p> Phone Number:
+        <input name='phone_number' type='text' placeholder='Write the student phone number here...'>
+    </p>
+
+    <p> Where will the student study?</p>
+    <select>
+        <option value=''>Select here the college addres:</option>
+        <option value='1'>Asa Norte</option>
+        <option value='2'>Asa Sul</option>
+        <option value='1'>Sudoeste</option>
+    </select>
+
+    <p>Write some comments or additional information about the student</p>
+    <textarea name='comments'></textarea>
+    <br>
+
+    <button type='submit'>SEND YOUR ANSWERS HERE</button>
+
+</form>
---
+++ resources/views/site/layouts/_partials/basic-html.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Og's App - @yield('title')</title>
+</head>
+<body>
+
+    <h1>Welcome to my APP!</h1>
+    <p> Let's learn Laravel together! </p>
+    
+    @yield('content')
+
+
+</body>
+</html>
