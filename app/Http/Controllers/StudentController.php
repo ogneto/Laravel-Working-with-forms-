c580344ca97daab64534bc798b32055939b45600
OgNeto <ogsilvanetu@gmail.com>
2025-10-07 14:26:55 -0300

    Create a student using the methods create and fill

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -24,6 +24,17 @@ class StudentController extends Controller
 
         $student->save();
 
+        /* Other forms to save a student in the database:
+
+            // Using the fill method:
+            $student = new Student;
+            $student->fill($request->all());
+            $student->save();
+
+            // Using the create mehotd:
+            $student = new Student;
+            $student->create($request->all());
+        */
         return view('site.form-ok', ['request' => $request]);
     }
 }
